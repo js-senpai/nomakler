884787600aa5aa79357eeb3fad974dfa70b8fd46
Andrew <fincheran@gmail.com>
2021-11-12 23:06:55 +0200

    add small fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const nextTranslate = require('next-translate')
-const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
-const path = require('path')
 module.exports = withPlugins([
   nextTranslate
 ],{
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new DuplicatePackageCheckerPlugin())
-    config.resolve.alias['fast-deep-equal'] = path.resolve(
-        __dirname,
-        'node_modules',
-        'fast-deep-equal'
-    )
-    return config
-  },
   reactStrictMode: true,
   compress: true,
   swcMinify: true,
