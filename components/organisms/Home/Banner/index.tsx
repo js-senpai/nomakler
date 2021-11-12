aa7cbbc509e74d82b990dc17084bd3f56af00f77
Andrew <fincheran@gmail.com>
2021-11-12 17:58:39 +0200

    add small fixes

--- components/organisms/Home/Banner/index.tsx
+++ components/organisms/Home/Banner/index.tsx
@@ -1,14 +1,23 @@
 import dynamic from "next/dynamic";
 import styles from './Banner.module.sass'
 import {BannerProps} from "./Banner.props";
+import Image from "next/image";
 // Import components
 const DefaultContainer = dynamic(() =>
     import('../../../containers/DefaultContainer')
 )
 const Banner = ({img = '/images/organisms/Home/Banner/banner.jpg',title = '',description = '',...props}:BannerProps):JSX.Element => {
     return(
-        <section  className={styles.banner} style={{backgroundImage: `url(${img})`}}>
-            <DefaultContainer>
+        <section  className={styles.banner}>
+            <Image
+                src={img}
+                alt={title}
+                quality="50"
+                layout="fill"
+                object-fit="cover"
+                objectPosition="center"
+            />
+            <DefaultContainer className="relative">
                 {title.length?<h1 className={styles.banner__title}>{title}</h1>: null}
                 {description.length?<div className={styles.banner__description}>{description}</div>: null}
             </DefaultContainer>
