b9310d97a747e6930ec1131a072fcbe173e04659
Andrew <fincheran@gmail.com>
2021-11-15 15:54:57 +0200

    add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,11 +10,7 @@ export default class MyDocument extends Document{
             <Html lang={this.props.locale}>
                 <Head>
                     <meta charSet="utf-8" />
-                    <meta name="theme-color" content="#000000" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap"
-                          rel="stylesheet" />
+                    <meta name="theme-color" content="#fff" />
                 </Head>
                 <body>
                     <Main />
