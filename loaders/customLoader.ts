c52f0fcda897c8e6044c4c946d6d249a34fa735d
Andrew <fincheran@gmail.com>
2021-11-16 14:36:31 +0200

    test commit

--- loaders/customLoader.ts
+++ loaders/customLoader.ts
@@ -1,5 +1,6 @@
-// @ts-ignore
-const customLoader = ({ src, width, quality }) => {
+import {ImageLoaderProps} from "next/image";
+
+const customLoader = ({ src, width, quality }:ImageLoaderProps) => {
     return `${src}?w=${width}&q=${quality || 75}`;
 };
 export default customLoader
