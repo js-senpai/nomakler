d806fd6f5bc6ba637d68eba2fab38f349da7072b
Andrew <fincheran@gmail.com>
2021-11-16 14:18:05 +0200

    test commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 // const nextTranslate = require('next-translate')
 module.exports = withPlugins([
-  // nextTranslate
+  [
+    optimizedImages,
+    {
+      // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+      optimizeImages: false,
+    },
+  ],
 ],{
   reactStrictMode: true,
   compress: true,
   swcMinify: true,
   images: {
+    disableStaticImages: true,
     domains: ['goofy-wilson-872921.netlify.app'],
   },
 });
